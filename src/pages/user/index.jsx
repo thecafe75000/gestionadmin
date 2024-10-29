9ab481f29ee8a6f32f8dcd8fec192f826be53860
LEI <lei@Laptop-LEI-MAC.local>
2024-10-29 18:28:12 +0100

    part of form table

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Form, Input, Table, Popconfirm} from 'antd'
+import { Button, Form, Input, Table, Popconfirm, Modal, InputNumber, Select, DatePicker} from 'antd'
 import './index.css'
 import { getUser } from '@/api'
 
 const User = () => {
   const [userlistData, setUserlistData] = useState({ name: '' })
   const [tableData, setTableData] = useState([])
+  const [modalType, setModalType] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [form] = Form.useForm()
   
   const getTableData = async () => {
     const result = await getUser(userlistData)
@@ -19,40 +22,47 @@ const User = () => {
 
   const columns = [
     {
-      title: 'Username',
-      dataIndex: 'name',
+      title: 'Nom de Client',
+      dataIndex: 'name'
     },
     {
       title: 'Age',
-      dataIndex: 'age',
+      dataIndex: 'age'
     },
     {
-      title: 'Gender',
+      title: 'Genre',
       dataIndex: 'sex',
       render: (val) => {
         return val ? 'female' : 'male'
       }
     },
     {
-      title: 'Birthday',
-      dataIndex: 'birth',
+      title: 'Anniversaire',
+      dataIndex: 'birth'
     },
     {
-      title: 'Address',
-      dataIndex: 'addr',
+      title: 'Adresse',
+      dataIndex: 'addr'
     },
     {
       title: 'Operation',
       render: (rowData) => {
         return (
           <div className='operation'>
-            <Button style={{ marginRight: '5px' }} onClick={() => handleClick('edit', rowData)}>Edit</Button>
-            <Popconfirm
-              title='Warn'
-              description='This operation will delete the user, are you sure?'
-              onConfirm={()=>handleDelete(rowData)}
+            <Button
+              style={{ marginRight: '5px' }}
+              onClick={() => handleClick('edit', rowData)}
             >
-               <Button type='primary' danger>Delete</Button>
+              Modifier
+            </Button>
+            <Popconfirm
+              title='Avertir'
+              description='Cette opération supprimera ce client, êtes-vous sûr?'
+              onConfirm={() => handleDelete(rowData)}
+            >
+              <Button type='primary' danger>
+                Supprimer
+              </Button>
             </Popconfirm>
           </div>
         )
@@ -66,7 +76,12 @@ const User = () => {
   }, [])
 
   const handleClick = (type, data) => {
-    
+    setIsModalOpen(!isModalOpen)
+    if (type === 'add') {
+      setModalType(0)
+    } else {
+       setModalType(1)
+    }
   }
 
   const handleFinish = (e) => {
@@ -74,6 +89,14 @@ const User = () => {
       name: e.name
     })
     console.log('user page', e)
+  }
+
+  const handleOk = () => {
+    
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
   }
 
   return (
@@ -94,6 +117,100 @@ const User = () => {
         </Form>
       </div>
       <Table columns={columns} dataSource={tableData} rowKey={'id'} />
+      <Modal
+        open={isModalOpen}
+        title={modalType ? 'modifier' : '+ new user'}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form
+          form={form}
+          labelCol={{
+            span: 6
+          }}
+          wrapperCol={{
+            span: 18
+          }}
+          labelAlign='left'
+        >
+          <Form.Item
+            label='Nom'
+            name='username'
+            rules={[
+              {
+                required: true,
+                message: 'Please enter username'
+              }
+            ]}
+          >
+            <Input placeholder='Please enter username' />
+          </Form.Item>
+          <Form.Item
+            label='Age'
+            name='age'
+            rules={[
+              {
+                required: true,
+                message: 'Please enter age'
+              },
+              {
+                type: 'number',
+                message: 'Age must be a number'
+              }
+            ]}
+          >
+            <InputNumber placeholder='enter age' />
+          </Form.Item>
+          <Form.Item
+            label='Genre'
+            name='sex'
+            rules={[
+              {
+                required: true,
+                message: 'Gender is required'
+              }
+            ]}
+          >
+            <Select
+              placeholder='please choose gender'
+              options={[
+                {
+                  value: 0,
+                  label: 'Male'
+                },
+                {
+                  value: 1,
+                  label: 'Female'
+                }
+              ]}
+            />
+          </Form.Item>
+          <Form.Item
+            label='Anniversaire'
+            name='birth'
+            rules={[
+              {
+                required: true,
+                message: 'Anniversaire is required'
+              }
+            ]}
+          >
+            <DatePicker placeholder='choose date' format='DD/MM/YYYY' />
+          </Form.Item>
+          <Form.Item
+            label='Adresse'
+            name='addr'
+            rules={[
+              {
+                required: true,
+                message: 'Adresse is required'
+              }
+            ]}
+          >
+            <Input placeholder='enter adresse' />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   )
 }
